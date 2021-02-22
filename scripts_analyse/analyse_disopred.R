1d6bbd1a50af3d9ab293a83cd2af49d6d7604334
Lugulabre <pretetmael@sfr.fr>
2021-02-22 17:07:12 +0100

    script diso

--- scripts_analyse/analyse_disopred.R
+++ scripts_analyse/analyse_disopred.R
@@ -6,14 +6,17 @@ require(MASS)
 list_files = as.vector(read.table("list_files_diso.txt")[,1])
 
 list_diso_result = list()
-cpt = 1
+cpt = 2
 
-for (file in list_files) {
+list_diso_result[[1]] = read.table(list_files[1], skip = 3)
+name_prot = str_sub(list_files[1],1, 6)
+
+for (file in list_files[2:length(list_files)]) {
   list_diso_result[[cpt]] = read.table(file, skip = 3)
   cpt = cpt + 1
 }
 
 
-str_extract(list_files[1], 6)
+str_sub(list_files[1],1, 6)
 list_files[2]
 
